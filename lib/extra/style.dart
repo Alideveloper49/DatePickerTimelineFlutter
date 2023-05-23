5b700acd2a6122a2010431fead7fc14eca67e57f
Umair <32221510+ITUmair@users.noreply.github.com>
2023-05-23 18:49:03 +0500

    first commit

---
+++ lib/extra/style.dart
@@ -0,0 +1,39 @@
+import 'package:date_picker_timeline_trendway/extra/color.dart';
+import 'package:flutter/material.dart';
+import 'package:date_picker_timeline_trendway/extra/dimen.dart';
+
+const TextStyle defaultMonthTextStyle = TextStyle(
+  color: AppColors.defaultMonthColor,
+  fontSize: Dimen.monthTextSize,
+  fontWeight: FontWeight.w500,
+);
+
+const TextStyle defaultDateTextStyle = TextStyle(
+  color: AppColors.defaultDateColor,
+  fontSize: Dimen.dateTextSize,
+  fontWeight: FontWeight.w500,
+);
+
+const TextStyle defaultDayTextStyle = TextStyle(
+  color: AppColors.defaultDayColor,
+  fontSize: Dimen.dayTextSize,
+  fontWeight: FontWeight.w500,
+);
+
+const TextStyle disabledMonthTextStyle = TextStyle(
+  color: AppColors.defaultDisabledColor,
+  fontSize: Dimen.monthTextSize,
+  fontWeight: FontWeight.w500,
+);
+
+const TextStyle disabledDateTextStyle = TextStyle(
+  color: AppColors.defaultDisabledColor,
+  fontSize: Dimen.dateTextSize,
+  fontWeight: FontWeight.w500,
+);
+
+const TextStyle disabledDayTextStyle = TextStyle(
+  color: AppColors.defaultDisabledColor,
+  fontSize: Dimen.dayTextSize,
+  fontWeight: FontWeight.w500,
+);
---
+++ test/date_picker_timeline_test.dart
@@ -0,0 +1,5 @@
+import 'package:flutter_test/flutter_test.dart';
+
+import 'package:date_picker_timeline_trendway/date_picker_timeline.dart';
+
+void main() {}
